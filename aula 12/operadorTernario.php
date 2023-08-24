6b9a050ce3fa28d92482131de662c566f10ba682
Lucila <105459895+lucilafmoraes@users.noreply.github.com>
2023-08-23 23:18:37 -0300

    Delete aula 12 directory

--- aula 12/operadorTernario.php
+++
@@ -1,9 +0,0 @@
-<?php
-//expressão ? valor_caso_verdadeiro : valor_caso_falso
-//aspas duplas conseguem interpretar variaveis
-
-$c = ($a > $b) ? 'Maior' : 'Menor ou igual';
-
-
-$tratamento = ('M'== $sexo) ? 'Sr.' : 'Srª';
-?>
